b3bc3ef9d43addbaacb37483b775aa8d085749b6
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-01-30 20:24:46 +0530

    complete the project

--- src/Pages/ContainerPage/ContainerPage.jsx
+++ src/Pages/ContainerPage/ContainerPage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  Container,
+} from "@mui/material";
 
 const ContainerPage = () => {
-  return <div>ContainerPage</div>;
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Card sx={{ width: 350, boxShadow: 3, borderRadius: 2 }}>
+        <CardHeader title="Welcome to Asgardeo Auth App" />
+        <CardContent>
+          <Typography variant="body1">
+            You have successfully logged in. You can Access this Page
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default ContainerPage;
